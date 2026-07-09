9ed1fca33dadf5d931df8fc7727567edbfa33142
ompatel245 <ombhavani7@gmail.com>
2026-07-09 18:26:20 +0530

    add main.py

---
+++ practice4/main2.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from pydantic import BaseModel , Field
+app = FastAPI()
+
+class users(BaseModel):
+    id : int = Field(alias="user id" ,ge= 0 , le=100 ,examples=[12])
+    name : str = Field(max_length=20 , min_length=6 , alias= "username" , examples=["om"])
+
+app.post("/")
+def create_user(data:users):
+    return {"message":data} 
