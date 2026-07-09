3d54b9188375e8c32e4b4692677404eb300a49b5
ompatel245 <ombhavani7@gmail.com>
2026-07-09 18:31:21 +0530

    Update main2.py

--- practice4/main2.py
+++ practice4/main2.py
@@ -4,8 +4,8 @@ app = FastAPI()
 
 class users(BaseModel):
     id : int = Field(alias="user id" ,ge= 0 , le=100 ,examples=[12])
-    name : str = Field(max_length=20 , min_length=6 , alias= "username" , examples=["om"])
+    name : str = Field(max_length=20 , min_length=2 , alias= "username" , examples=["om"])
 
-app.post("/")
+@app.post("/")
 def create_user(data:users):
     return {"message":data} 
